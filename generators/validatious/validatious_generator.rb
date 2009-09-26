8d260a373802c820c170dd43e625fd747094f3f7
Jonas Grimfelt <grimen@gmail.com>
2009-09-26 23:34:36 +0200

    Added v2.standalone.full.min.js to the generator. Updated TODO with I18n-notes.

--- generators/validatious/validatious_generator.rb
+++ generators/validatious/validatious_generator.rb
@@ -3,7 +3,7 @@ class ValidatiousGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.template 'v2.standalone.full.min.js', File.join('public', 'javascripts', 'v2.standalone.full.min.js')
-      m.template 'validatious_config.js', File.join('public', 'javascripts', 'validatious_config.js')
+      m.template 'validatious_config.js', File.join('public', 'javascripts', 'validatious.config.js')
     end
   end
   
