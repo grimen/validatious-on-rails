41ff71fde1143a7a07d67825baa421c672bfc408
Jonas Grimfelt <grimen@gmail.com>
2009-09-27 17:47:13 +0200

    Making tests more readable with ActiveSupport::TestCase + ActionView::TestCase.

--- test/validatious/form_helper_test.rb
+++ test/validatious/form_helper_test.rb
@@ -7,62 +7,64 @@ module Test::Unit::Assertions
   # Assert that a piece of HTML includes the class name.
   #
   def assert_has_class(class_name, html, message = nil)
-    classes = /class="([^"]*)"/.match(html)[1].split(" ")
+    classes = /class="([^"]*)"/.match(html)[1].split(' ')
     full_message = build_message(message, "<?>\nexpected to include class(es) <?>.\n", html, class_name)
 
     assert_block(full_message) do
-      class_name.split(" ").all? { |cname| classes.include?(cname) }
+      class_name.split(' ').all? { |cname| classes.include?(cname) }
     end
   end
 
 end
 
-class FormHelperTest < Test::Unit::TestCase
+require 'action_view/test_case'
+
+class FormHelperTest < ::ActionView::TestCase
 
   include ActionView::Helpers::FormHelper
 
-  def test_required_text_field
-    assert_has_class "required", text_field(:bogus_item, :name)
-    assert_has_class "required text", text_field(:bogus_item, :name, :class => "text")
+  test "required :text_field" do
+    assert_has_class 'required', text_field(:bogus_item, :name)
+    assert_has_class 'required text', text_field(:bogus_item, :name, :class => 'text')
   end
 
-  def test_required_password_field
+  test "required :password_field" do
     assert_has_class "required", password_field(:bogus_item, :name)
     assert_has_class "required text", password_field(:bogus_item, :name, :class => "text")
   end
 
-  def test_required_text_area
-    assert_has_class "required", text_field(:bogus_item, :body)
-    assert_has_class "required text", text_field(:bogus_item, :body, :class => "text")
+  test "required :text_area" do
+    assert_has_class "required", text_area(:bogus_item, :body)
+    assert_has_class "required text", text_area(:bogus_item, :body, :class => "text")
   end
 
-  def test_required_check_box # a.k.a. "acceptance required"
-    assert_has_class "required", text_field(:bogus_item, :signed)
-    assert_has_class "required boolean", text_field(:bogus_item, :signed, :class => "boolean")
+  test "required :check_box" do # a.k.a. "acceptance required"
+    assert_has_class "required", check_box(:bogus_item, :signed)
+    assert_has_class "required boolean", check_box(:bogus_item, :signed, :class => "boolean")
   end
 
-  def test_required_radio_button
+  test "required :radio_button" do
     # TODO
   end
 
-  def test_confirmation_field
+  test "confirmation_of-field" do
     # TODO: Add form-builder-test as well
     assert_has_class "confirmation-of_name", text_field(:bogus_item, :name_confirmation)
     assert_has_class "confirmation-of_name text", text_field(:bogus_item, :name_confirmation, :class => "text")
   end
 
-  def test_normal_label
+  test "regular label" do
     assert_equal "<label for=\"bogus_item_name\">Name</label>", label(:bogus_item, :name)
   end
 
-  def test_label_with_title
+  test "label with title" do
     assert_equal "<label for=\"bogus_item_name\" title=\"craaazy\">Name</label>",
                  label(:bogus_item, :name, nil, :title => "craaazy")
   end
 
-  def test_label_without_title
+  test "label without title" do
     assert_equal "<label for=\"bogus_item_name\" title=\"Name\">Your name</label>",
                  label(:bogus_item, :name, "Your name")
   end
-  
+
 end
--- test/validatious/rails_validation_test.rb
+++ test/validatious/rails_validation_test.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper'))
 
-class RailsValidationTest < Test::Unit::TestCase
+require 'active_support/test_case'
+
+class RailsValidationTest < ::ActiveSupport::TestCase
   
   include ActionView::Helpers::FormHelper
   
@@ -12,36 +14,36 @@ class RailsValidationTest < Test::Unit::TestCase
     ActiveRecord::Reflection::MacroReflection.new(macro, :name, options, BogusItem.new)
   end
   
-  def test_acceptance_of
+  test "acceptance_of" do
     validation = Validatious::RailsValidation.acceptance_of(validation(:validates_acceptance_of))
     assert_equal 'required', validation[:class]
   end
   
-  def test_associated
+  test "associated" do
     # TODO: not implemented
   end
   
-  def test_confirmation_of
+  test "confirmation_of" do
     validation = Validatious::RailsValidation.confirmation_of(validation(:validates_confirmation_of))
     assert_equal 'confirmation-of_name', validation[:class]
   end
   
-  def test_exclusion_of
+  test "exclusion_of" do
     # TODO: not implemented
   end
   
-  def test_format_of
+  test "format_of" do
     # pattern = /^(http|https|ftp):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i
     # validation = Validatious::RailsValidation.format_of(validation(:validates_format_of, :with => pattern, :name => "email"))
     # assert_equal 'email', validation[:class]
     # TODO: Not fully supported really, should 
   end
   
-  def test_inclusion_of
+  test "inclusion_of" do
     # TODO: not implemented
   end
   
-  def test_length_of_with_in
+  test "length_of_with_in" do
     validation = Validatious::RailsValidation.length_of(validation(:validates_length_of, :in => 1..10))
     assert_equal 'min-length_1 max-length_10', validation[:class]
   
@@ -49,7 +51,7 @@ class RailsValidationTest < Test::Unit::TestCase
     assert_equal 'min-length_1 max-length_9', validation[:class]
   end
   
-  def test_length_of_with_within
+  test "length_of_with_within" do
     validation = Validatious::RailsValidation.length_of(validation(:validates_length_of, :within => 1..10))
     assert_equal 'min-length_1 max-length_10', validation[:class]
   
@@ -57,32 +59,32 @@ class RailsValidationTest < Test::Unit::TestCase
     assert_equal 'min-length_1 max-length_9', validation[:class]
   end
   
-  def test_length_of_with_minimum
+  test "length_of_with_minimum" do
     validation = Validatious::RailsValidation.length_of(validation(:validates_length_of, :minimum => 1))
     assert_equal 'min-length_1', validation[:class]
   end
   
-  def test_length_of_with_maximum
+  test "length_of_with_maximum" do
     validation = Validatious::RailsValidation.length_of(validation(:validates_length_of, :maximum => 1))
     assert_equal 'max-length_1', validation[:class]
   end
   
-  def test_length_of_with_min_and_max
+  test "length_of_with_min_and_max" do
     validation = Validatious::RailsValidation.length_of(validation(:validates_length_of, :minimum => 1, :maximum => 3))
     assert_equal 'min-length_1 max-length_3', validation[:class]
   end
   
-  def test_numericality_of
+  test "numericality_of" do
     validation = Validatious::RailsValidation.numericality_of(validation(:validates_numericality_of))
     assert_equal 'numeric', validation[:class]
   end
   
-  def test_presence_of
+  test "presence_of" do
     validation = Validatious::RailsValidation.presence_of(validation(:validates_presence_of))
     assert_equal 'required', validation[:class]
   end
   
-  def test_uniqueness_of
+  test "uniqueness_of" do
     # TODO: not implemented
   end
   
