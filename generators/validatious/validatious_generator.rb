548d79bec5ca451460190045dad71d37caed1cf7
Jonas Grimfelt <grimen@gmail.com>
2009-09-27 16:31:18 +0200

    Fixed generator-bug (couldn't find template), and cleaned up the generator README-sample a bit.

--- generators/validatious/validatious_generator.rb
+++ generators/validatious/validatious_generator.rb
@@ -5,7 +5,7 @@ class ValidatiousGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.template 'v2.standalone.full.min.js', File.join('public', 'javascripts', 'v2.standalone.full.min.js')
-      m.template 'validatious_config.js', File.join('public', 'javascripts', 'validatious.config.js')
+      m.template 'validatious.config.js', File.join('public', 'javascripts', 'validatious.config.js')
     end
   end
   
