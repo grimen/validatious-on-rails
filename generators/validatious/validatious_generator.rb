8930c990b622b1f96e76b5708399795ccb716edd
grimen <grimen@gmail.com>
2009-10-21 17:05:27 +0200

    Fixed typo in the generator. closes #11

--- generators/validatious/validatious_generator.rb
+++ generators/validatious/validatious_generator.rb
@@ -4,11 +4,11 @@ class ValidatiousGenerator < Rails::Generator::Base
   
   def manifest
     record do |m|
-      m.template 'initializer.rb',            File.join(*%[config initializers validatious-on-rails.rb])
-      m.template 'XMLHttpRequest.js',         File.join(*%[public javascripts XMLHttpRequest.js])
-      m.template 'v2.standalone.full.min.js', File.join(*%[public javascripts v2.standalone.full.min.js])
-      m.template 'v2.config.js',              File.join(*%[public javascripts v2.config.js])
-      m.template 'v2.rails.js',               File.join(*%[public javascripts v2.rails.js])
+      m.template 'initializer.rb',            File.join(*%w[config initializers validatious-on-rails.rb])
+      m.template 'XMLHttpRequest.js',         File.join(*%w[public javascripts XMLHttpRequest.js])
+      m.template 'v2.standalone.full.min.js', File.join(*%w[public javascripts v2.standalone.full.min.js])
+      m.template 'v2.config.js',              File.join(*%w[public javascripts v2.config.js])
+      m.template 'v2.rails.js',               File.join(*%w[public javascripts v2.rails.js])
     end
   end
   
